83c3b25bf6f4207c62e7fb5e90132ba3aa979b7a
Sriya Pratipati <pratipat@usc.edu>
2025-04-22 17:53:40 +0000

    improve runtime for worlde

--- wordle.cpp
+++ wordle.cpp
@@ -6,6 +6,7 @@
 #include <map>
 #include <set>
 #include <vector>
+#include <unordered_map>
 #endif
 
 #include "wordle.h"
@@ -15,7 +16,7 @@ using namespace std;
 
 // Add prototypes of helper functions here
 // curr --> curr string we've built, floating--> available yellow chars, in-->fixed letters, place-->where we are in the string, results-->curr results set to be returned, dict-->same dict from other
-void helper(string curr, vector<char> floating, const string& in, int place, set<string>& results, const set<string>& dict);
+void helper(string curr, unordered_map<char, int>& floating, int floating_needed, const string& in, int place, set<string>& results, const set<string>& dict);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -24,48 +25,50 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     set<string> results;
-    string curr = "";
-    vector<char> flot;
+    string curr = in;
+    unordered_map<char, int> flot;
     for (int i = 0; i < floating.size(); i++) {
-      flot.push_back(floating[i]);
+      flot[floating[i]]++;
     }
-    helper(curr, flot, in, 0, results, dict);
+    helper(curr, flot, floating.length(), in, 0, results, dict);
     return results;
 
 }
 
 // Define any helper functions here
-void helper(string curr, vector<char> floating, const string& in, int place, set<string>& results, const set<string>& dict) {
+void helper(string curr, unordered_map<char, int>& floating, int floating_needed, const string& in, int place, set<string>& results, const set<string>& dict) {
   // after we're done recursively building, checking if it is a valid word
   if (place == in.size()) {
-    if (floating.empty() && dict.find(curr) != dict.end()) {
+    if (floating_needed == 0 && dict.find(curr) != dict.end()) {
       results.insert(curr);
       //cout << "Added to results" << endl;
     }
     return;
   }
-  if (floating.size() > (in.size() - place)) {
+  if (floating_needed > (in.size() - place)) {
     return;
   }
   // if the letter is already found/fixed
-  else if (in[place] != '-') {
-    curr += in[place];
-    helper(curr, floating, in, place + 1, results, dict);
+  if (in[place] != '-') {
+    helper(curr, floating, floating_needed, in, place + 1, results, dict);
   }
   // otherwise, recursively trying letters
   else {
     // iterating through the floating characters **priority
-    for (int i = 0; i < floating.size(); i++) {
-      string newStr = curr + floating[i];
-      vector<char> temp = floating;
-      temp.erase(temp.begin() + i);
-      helper(newStr, temp, in, place + 1, results, dict);
-    }
-    // iterating through the remaining letters
-    string letters = "abcdefghijklmnopqrstuvwxyz";
-    for (int i = 0; i < letters.size(); i++) {
-        string newStr = curr + letters[i];
-        helper(newStr, floating, in, place + 1, results, dict);
+    for (char guess = 'a'; guess <= 'z'; guess++) {
+      bool is_floating = floating.count(guess) && floating[guess] > 0;
+      if (is_floating || floating_needed < (int)(in.size() - place)) {
+        if (is_floating) {
+          floating[guess]--;
+          floating_needed--;
+        }
+        curr[place] = guess;
+        helper(curr, floating, floating_needed, in, place + 1, results, dict);
+        if (is_floating) {
+          floating[guess]++;
+          floating_needed++;
+        }
+      }
     }
   }
 }
