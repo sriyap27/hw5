e7fa9f0b016819544c891406ef772bc6334d07c6
Sriya Pratipati <pratipat@usc.edu>
2025-04-22 17:16:36 +0000

    finished hw5

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,7 +21,7 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+bool helper(const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched, int day, int workers, map<Worker_T, int>& freq);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -37,9 +37,44 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
-
+  sched.resize(avail.size(), vector<Worker_T>(dailyNeed, INVALID_ID));
+  map<Worker_T, int> freq;
+  return helper(avail, dailyNeed, maxShifts, sched, 0, 0, freq);
 
 
 
 }
+bool helper(const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched, int day, int workers, map<Worker_T, int>& freq) {
+  if (day >= avail.size() && workers == 0) {
+    return true;
+  }
+  for (unsigned int i = 0; i < avail[day].size(); i++) {
+    if (avail[day][i]) {
+      if (freq[i] < maxShifts) {
+        bool assigned = false;
+        for (unsigned int j = 0; j < workers; j++) {
+          if (sched[day][j] == i) {
+            assigned = true;
+            break;
+          }
+        }
+        if (assigned) continue;
+        if (!assigned) {
+          sched[day][workers] = i;
+          freq[i]++;
+        }
+        if (workers + 1 == dailyNeed) {
+          if (helper(avail, dailyNeed, maxShifts, sched, day + 1, 0, freq)) return true;
+        }
+        else {
+          if (helper(avail, dailyNeed, maxShifts, sched, day, workers + 1, freq)) return true;
+        }
+        freq[i]--;
+        sched[day][workers] = INVALID_ID;
+      }
+    }
+  }
+  return false;
+}
+
 
--- wordle.cpp
+++ wordle.cpp
@@ -5,6 +5,7 @@
 #include <algorithm> 
 #include <map>
 #include <set>
+#include <vector>
 #endif
 
 #include "wordle.h"
@@ -13,7 +14,8 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+// curr --> curr string we've built, floating--> available yellow chars, in-->fixed letters, place-->where we are in the string, results-->curr results set to be returned, dict-->same dict from other
+void helper(string curr, vector<char> floating, const string& in, int place, set<string>& results, const set<string>& dict);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -21,8 +23,49 @@ std::set<std::string> wordle(
     const std::string& floating,
     const std::set<std::string>& dict)
 {
-    // Add your code here
+    set<string> results;
+    string curr = "";
+    vector<char> flot;
+    for (int i = 0; i < floating.size(); i++) {
+      flot.push_back(floating[i]);
+    }
+    helper(curr, flot, in, 0, results, dict);
+    return results;
 
 }
 
 // Define any helper functions here
+void helper(string curr, vector<char> floating, const string& in, int place, set<string>& results, const set<string>& dict) {
+  // after we're done recursively building, checking if it is a valid word
+  if (place == in.size()) {
+    if (floating.empty() && dict.find(curr) != dict.end()) {
+      results.insert(curr);
+      //cout << "Added to results" << endl;
+    }
+    return;
+  }
+  if (floating.size() > (in.size() - place)) {
+    return;
+  }
+  // if the letter is already found/fixed
+  else if (in[place] != '-') {
+    curr += in[place];
+    helper(curr, floating, in, place + 1, results, dict);
+  }
+  // otherwise, recursively trying letters
+  else {
+    // iterating through the floating characters **priority
+    for (int i = 0; i < floating.size(); i++) {
+      string newStr = curr + floating[i];
+      vector<char> temp = floating;
+      temp.erase(temp.begin() + i);
+      helper(newStr, temp, in, place + 1, results, dict);
+    }
+    // iterating through the remaining letters
+    string letters = "abcdefghijklmnopqrstuvwxyz";
+    for (int i = 0; i < letters.size(); i++) {
+        string newStr = curr + letters[i];
+        helper(newStr, floating, in, place + 1, results, dict);
+    }
+  }
+}
